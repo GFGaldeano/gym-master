7e6a81dd613c1385ffb295c3727436504290393c
Frysccou <franespindola71@gmail.com>
2025-07-01 15:13:12 -0300

    fix: error en vista de pagos y interfaz inadecuada

--- src/components/tables/PagoTable.tsx
+++ src/components/tables/PagoTable.tsx
@@ -13,17 +13,7 @@ import {
   TableRow,
   TableCaption,
 } from "@/components/ui/table";
-
-export interface Pago {
-  id: string;
-  socio_id: string;
-  cuota_id: string;
-  fecha_pago: string;
-  fecha_vencimiento: string;
-  monto_pagado: number;
-  total: number;
-  registrado_por: string;
-}
+import { ResponsePago } from "@/interfaces/pago.interface";
 
 export default function PagoTable({
   pagos,
@@ -32,17 +22,17 @@ export default function PagoTable({
   onView,
   onDelete,
 }: {
-  pagos: Pago[];
+  pagos: ResponsePago[];
   loading?: boolean;
-  onEdit: (pago: Pago) => void;
-  onView?: (pago: Pago) => void;
-  onDelete?: (pago: Pago) => void;
+  onEdit: (pago: ResponsePago) => void;
+  onView?: (pago: ResponsePago) => void;
+  onDelete?: (pago: ResponsePago) => void;
 }) {
   if (loading) {
     return (
       <div className="space-y-2">
         {[...Array(5)].map((_, i) => (
-          <Skeleton key={i} className="h-9 w-full rounded-md" />
+          <Skeleton key={i} className="w-full rounded-md h-9" />
         ))}
       </div>
     );
@@ -50,14 +40,14 @@ export default function PagoTable({
 
   if (pagos.length === 0 && !loading) {
     return (
-      <div className="text-center text-muted-foreground py-10">
+      <div className="py-10 text-center text-muted-foreground">
         No hay pagos registrados aún.
       </div>
     );
   }
 
   return (
-    <Table className="w-full text-sm border border-border rounded-md overflow-hidden">
+    <Table className="w-full overflow-hidden text-sm border rounded-md border-border">
       <TableHeader>
         <TableRow className="bg-muted/50 text-muted-foreground">
           <TableHead>Socio</TableHead>
@@ -76,13 +66,15 @@ export default function PagoTable({
             key={i}
             className="odd:bg-muted/40 hover:bg-[#a8d9f9] transition-colors"
           >
-            <TableCell className="font-medium">{p.socio_id}</TableCell>
-            <TableCell>{p.cuota_id}</TableCell>
+            <TableCell className="font-medium">
+              {p.socio.nombre_completo}
+            </TableCell>
+            <TableCell>{p.cuota.descripcion}</TableCell>
             <TableCell>{p.fecha_pago}</TableCell>
             <TableCell>{p.fecha_vencimiento}</TableCell>
             <TableCell>${p.monto_pagado}</TableCell>
             <TableCell>${p.total}</TableCell>
-            <TableCell>{p.registrado_por}</TableCell>
+            <TableCell>{p.registrado_por.nombre}</TableCell>
             <TableCell className="flex gap-2">
               <Button
                 size="sm"
